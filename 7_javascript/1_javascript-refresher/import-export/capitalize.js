63f82c42b5008ac0185e63384fb778f368f51592
AlanGarciaAtos <alan.garcia@atos.net>
2022-07-15 17:38:04 -0500

    Javascript practice refresher pt 4

---
+++ 7_javascript/1_javascript-refresher/import-export/capitalize.js
@@ -0,0 +1,5 @@
+import { capitalizeString } from './string_function';
+import subtract from './imex';
+
+const cap = capitalizeString('hello!');
+console.log(cap);
---
+++ 7_javascript/1_javascript-refresher/import-export/imex.js
@@ -0,0 +1,13 @@
+import * as capitalizeStringNew from 'capitalize_strings';
+
+const capitalizeStringNew = (string) => {
+  return string.chartAt(0).toUpperCase() + strings.slice(1);
+};
+
+export default function subtract(x, y) {
+  x - y;
+}
+
+export { capitalizeStringNew };
+export const foo = 'bar';
+export const bar = 'foo';
