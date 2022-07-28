f14682ddd32dabf26825d3888ad5f30638e3a2c5
AlanGarciaAtos <alan.garcia@atos.net>
2022-07-27 23:43:32 -0500

    Project course pt 3

---
+++ 12_angular/project-course/recipe-app-course/src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+
+  // public name:string;
+  // public amount:number;
+
+  //Shorthand of above and we don't need to use this
+  constructor(public name: string, public amount: number) {
+
+  }
+
+}
